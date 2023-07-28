7840cc6e87abd03eebe62d3f15c6975f0b3665f6
ssszZ-TH <spachitralada@gmail.com>
2023-07-28 09:29:46 +0700

    ลืม invert fourier

--- unblur.py
+++ unblur.py
@@ -19,4 +19,12 @@ if __name__ == "__main__":
     img=cv.imread("./mushroom.png",cv.IMREAD_GRAYSCALE)
     gaussian = getgaussian()
     img_freq = fr.imgToFrequency(img)
-    gaussian_freq
+    gaussian_freq = fr.filterToFrequency(gaussian)
+    blur_img = img_freq * gaussian_freq
+    repaired_img = img_freq / gaussian_freq
+    
+    
+    
+    cv.imshow("original",img)
+    cv.imshow("blur",blur_img)
+    cv.imshow("repaired",repaired_img)
